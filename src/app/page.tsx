08df1970c6ea59c8e389d40db914bde136c49340
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-04-14 21:44:39 +0530

    initial test changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Made with love ❤❤❤ by Vinay Kumar
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
